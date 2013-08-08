79f03ecf9b1b3901de7051f0eee8040b49c4310d
Naomi Sorbet + Ben Seligman <naomisorbet@gmail.com + seligman.ben@gmail.com>
2013-08-08 16:46:14 -0700

    add cat rental double booking validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,23 +1,29 @@
 class CatRentalRequest < ActiveRecord::Base
   attr_accessible :begin_date, :cat_id, :end_date, :status
-  validates :end_date, :numericality => {greater_than: :begin_date}
   validates :status, :inclusion => ['undecided', 'approved', 'denied']
+  validate :validate_range
   validate :cannot_overlap
 
 
 
-  # def valid_date_range?
-#     self.begin_date < self.end_date
-#   end
+  private
 
   def overlaps?(other)
-    other.begin_date > self.end_date || self.begin_date > other.end_date
+    other.begin_date.between?(self.begin_date, self.end_date) ||
+    self.begin_date.between?(other.begin_date, other.end_date)
   end
 
   def cannot_overlap
-    other_rentals_of_cat = CatRentalRequest.where("cat_id = ?", @cat_id)
+    other_rentals_of_cat = CatRentalRequest.where("cat_id = ?", self.cat_id)
+
     if other_rentals_of_cat.any? { |rental| self.overlaps?(rental) }
-      errors[:overlap] << "#{self.name} already rented during this time."
+      errors[:overlap] << ":-( this cat is already rented during this time."
+    end
+  end
+
+  def validate_range
+    if self.begin_date > self.end_date
+      errors[:invalid_range] << "end date precedes begin date."
     end
   end
 end
